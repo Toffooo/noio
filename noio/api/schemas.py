6a4a2b59b1f7b5bb515d002c6cd65b24ef446f19
Kel0 <rozovdima123@gmail.com>
2021-03-05 10:28:38 +0600

    #1 Add chrome extensio

--- noio/api/schemas.py
+++ noio/api/schemas.py
@@ -43,3 +43,4 @@ class NoticeSchema(NoticeBase):
     id: int
     created_at: datetime
     updated_at: datetime
+    is_new: bool
--- noioapi.py
+++ noioapi.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from noio.api.router import v1
 
 
 api = FastAPI()
-
-api.include_router(router=v1, prefix="/api/v1")
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+api.include_router(
+    router=v1,
+    prefix="/api/v1"
+)
